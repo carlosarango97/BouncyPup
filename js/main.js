43772ef2a2cc44c0e6953513d1813abe931d8cf0
carlosarango97 <47753930+carlosarango97@users.noreply.github.com>
2019-08-11 21:22:20 -0500

    Wireframes and elements
    I modified the wireframes and I finished putting all the elements of the screen game

--- js/main.js
+++ js/main.js
@@ -30,10 +30,40 @@ const bg={
     }
 }
 
+const fg={
+    sX: 0,
+    sY: 281,
+    w: 295,
+    h: 170,
+    x: 0,
+    y: cvs.height - 40,
+    ch: 45,
+    draw: function(){
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.ch);       
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.ch);   
+    }
+}
+
+const character={
+    sX: 290,
+    sY: 99,
+    w: 98,
+    h: 101,
+    x:  50,
+    y:  40,
+    ch: 14,
+    cw: 57,
+    draw: function(){
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.cw, this.ch);       
+    }
+}
+
 function draw(){
     ctx.fillStyle =" #70c5ce";
     ctx.fillRect(0, 0, cvs.width, cvs.height);    
     bg.draw();
+    fg.draw();
+    character.draw();
 }
 
 function update(){
