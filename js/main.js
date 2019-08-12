176f5eed8c905ef2beb5b63d9b8636b3126ac4a1
carlosarango97 <47753930+carlosarango97@users.noreply.github.com>
2019-08-11 20:43:02 -0500

    Implements the background
    I implement the background of the game, but I used the wireframes insted of the original designs

--- js/main.js
+++ js/main.js
@@ -7,3 +7,43 @@ function page(pageOut, pageIn){
     document.getElementById(pageIn).classList.remove("ocultar");
     window.scroll(0,document.getElementById(pageIn).scrollTop);
 }
+
+const cvs = document.getElementById("level-1-canvas");
+const ctx = cvs.getContext("2d");
+
+let frames = 0;
+
+const sprite = new Image();
+sprite.src = "img/level1.png";
+
+const bg={
+    sX : 0,
+    sY : 0,
+    w : 275,
+    h : 224,
+    x : 0,
+    ch : 59,
+    y : cvs.height - 59,
+    draw: function(){
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.ch);       
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.ch);   
+    }
+}
+
+function draw(){
+    ctx.fillStyle =" #70c5ce";
+    ctx.fillRect(0, 0, cvs.width, cvs.height);    
+    bg.draw();
+}
+
+function update(){
+
+}
+
+function loop(){
+    update();
+    draw();
+    frames++;
+    requestAnimationFrame(loop);
+}
+loop();
