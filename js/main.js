be8fb09908c4dc5f4d61170972f07d2bd52e4eaf
carlosarango97 <47753930+carlosarango97@users.noreply.github.com>
2019-08-12 14:35:42 -0500

    States, gravity and animation
    First I worked in the changes of states (getReady, game and game over) also I added the gravity and the flap for the character and for the last I did the character animation

--- js/main.js
+++ js/main.js
@@ -45,29 +45,92 @@ const fg={
 }
 
 const character={
-    sX: 290,
-    sY: 99,
-    w: 98,
-    h: 101,
+    animation:[
+        {sX: 290, sY: 99, w: 98, h: 101, ch: 14, cw: 57},
+        {sX: 292, sY: 207, w: 95, h: 62, ch: 12, cw: 57}
+    ],   
+    y: 40, 
     x:  50,
-    y:  40,
-    ch: 14,
-    cw: 57,
+    frame: 0,
+    speed: 0,
+    jump: 4.6,
+    gravity: 0.25,
     draw: function(){
-        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.cw, this.ch);       
+        let charact = this.animation[this.frame];
+        this.y += this.frame == 1 ? 2 :0;
+        ctx.drawImage(sprite, charact.sX, charact.sY, charact.w, charact.h, this.x, this.y, charact.cw, charact.ch);       
+        this.y -= this.frame == 1 ? 2 :0;
+    },
+
+    flap: function(){
+        this.speed -= this.jump;
+    },
+
+    update: function(){
+        this.frame += frames % 10 == 0 ? 1:0;
+        this.frame = this.frame % this.animation.length;
+        if(state.current == state.getReady){
+
+        }else{
+            this.speed += this.gravity;
+            this.y += this.speed;
+            if(this.y+this.animation[this.frame].ch/2>=cvs.height-fg.ch){
+                this.y=cvs.height-fg.ch-this.animation[this.frame].ch/2;
+                if(state.current==state.game){
+                    state.current = state.gameOver;
+                }
+            }
+        }
     }
 }
+
+const over={
+    sX: 0,
+    sY: 480,
+    w: 231,
+    h: 53,
+    x: 70,
+    y: 50,
+    ch: 14,
+    cw: 171,
+    draw: function(){
+        if(state.current == state.gameOver)
+            ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.cw, this.ch);       
+    }
+}
+
+const state = {
+    current : 0,
+    getReady: 0,
+    game: 1,
+    gameOver: 2
+}
+
+cvs.addEventListener("click",function(evt){
+    switch(state.current){
+        case state.getReady:
+            state.current = state.game;
+            break;
+        case state.game:
+            character.flap();
+            break;
+        case state.gameOver:
+            state.current = state.getReady;
+            break;
+    }
+});
 
 function draw(){
     ctx.fillStyle =" #70c5ce";
     ctx.fillRect(0, 0, cvs.width, cvs.height);    
     bg.draw();
     fg.draw();
-    character.draw();
+    character.draw();    
+    over.draw();
 }
 
 function update(){
-
+    character.update();
 }
 
 function loop(){
