44133dda9bae848a13ca6d1f9e134424bc216731
carlosarango97 <47753930+carlosarango97@users.noreply.github.com>
2019-08-13 08:44:29 -0500

    Designs implementation
    I added some designs delivered by the designer and I implemented them into the game. Also I modified the action listener for the use with tap mobile, finally I changed the screen size for a better experience with a smartphone

--- js/main.js
+++ js/main.js
@@ -8,6 +8,8 @@ function page(pageOut, pageIn){
     window.scroll(0,document.getElementById(pageIn).scrollTop);
 }
 
+
+
 const cvs = document.getElementById("level-1-canvas");
 const ctx = cvs.getContext("2d");
 
@@ -16,40 +18,50 @@ let frames = 0;
 const sprite = new Image();
 sprite.src = "img/level1.png";
 
+const background = new Image();
+background.src = "img/fondoniv3.png";
+
+const dog = new Image();
+dog.src = "img/perroniv3.png";
+
+const imgObstacles = new Image();
+imgObstacles.src = "img/obstaculosniv3.png";
+
 const bg={
-    sX : 0,
+    sX : 5,
     sY : 0,
-    w : 278,
-    h : 570,
+    w : 6395,
+    h : 2782,
     x : 0,
     ch : 150,
+    cw: 1080,
     y : 0,
     dx: 2,
     draw: function(){
-        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.ch);       
-        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.ch);   
+        ctx.drawImage(background, this.sX, this.sY, this.w, this.h, this.x, this.y, this.cw, this.ch);
+        ctx.drawImage(background, this.sX, this.sY, this.w, this.h, this.x+this.cw, this.y, this.cw, this.ch);
     },
     update: function(){
         if(state.current == state.game){
-            this.x = (this.x - this.dx) % this.w;
+            this.x = (this.x - this.dx) % this.cw;
         }
     }
 }
 
 const character={
-    sX: 290,
-    sY: 99,
-    w: 98,
-    h: 101,
-    ch: 14,
-    cw: 57, 
+    sX: 0,
+    sY: 0,
+    w: 1097,
+    h: 991,
+    ch: 20,
+    cw: 80, 
     y: 55, 
     x:  50,
     speed: 0,
-    jump: 1.4,
+    jump: 2.3,
     gravity: 0.08,
     draw: function(){
-        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.cw, this.ch);  
+        ctx.drawImage(dog, this.sX, this.sY, this.w, this.h, this.x, this.y, this.cw, this.ch);  
     },
 
     flap: function(){
@@ -65,10 +77,11 @@ const character={
                 this.y=cvs.height- 20;
                 if(state.current==state.game){
                     state.current = state.gameOver;
+                    obstacles.clear();
                 }
             }else{                
-            //     this.speed += this.gravity;
-            //     this.y += this.speed;
+                this.speed += this.gravity;
+                this.y += this.speed;
             }
         }
     }
@@ -92,50 +105,57 @@ const over={
 const obstacles = {
     position : [],
     bottom: {
-        sX: 557,
-        sY: 259,
-        w: 75,
-        h: 133,
-        ch: 35
+        sX: 7,
+        sY: 1483,
+        w: 412,
+        h: 826,
+        ch: 40,
+        cw: 100
     },
     top: {
-        sX: 488,
-        sY: 472,
-        w: 230,
-        h: 125,
-        ch: 33
+        sX: 0,
+        sY: 539,
+        w: 1205,
+        h: 619,
+        ch: 33,
+        cw: 150
     },
-    gap: 40,
-    maxYPos: 10,
+    gap: 15,
+    maxYPos: 2,
     dx: 2,
     draw: function(){
         for(let i = 0; i<this.position.length; i++){
             let p = this.position[i];
             let topYPos = p.y;
             let bottomYPos = p.y + this.top.ch + this.gap;
-            let bottomH = 130 - bottomYPos;
-            ctx.drawImage(sprite, this.top.sX, this.top.sY, this.top.w, this.top.h, (p.x-30), topYPos, this.top.w, this.top.ch);       
-            ctx.drawImage(sprite, this.bottom.sX, this.bottom.sY, this.bottom.w, this.bottom.h, p.x, bottomYPos, this.bottom.w, bottomH); 
+            let bottomH = 135 - bottomYPos;
+            ctx.drawImage(imgObstacles, this.top.sX, this.top.sY, this.top.w, this.top.h, (p.x-30), topYPos, this.top.cw, this.top.ch);       
+            ctx.drawImage(imgObstacles, this.bottom.sX, this.bottom.sY, this.bottom.w, this.bottom.h, p.x, bottomYPos, this.bottom.cw, bottomH); 
+            ctx.drawImage(imgObstacles, this.top.sX, this.top.sY, this.top.w, this.top.h, (p.x+40), (topYPos-50), this.top.cw, this.top.ch);          
         }
+    },
+
+    clear: function(){
+        this.position = [];
     },
 
     update: function(){
         if(state.current !== state.game) return;
         if(frames%150 == 0){
             this.position.push({
-                x: cvs.width,
-                y: this.maxYPos * (Math.random() + 1)
+                x: cvs.width,                
+                y: this.maxYPos * (Math.random()*18+2)
             });
         }        
         for(let i = 0; i<this.position.length; i++){
             let p = this.position[i];
             p.x -= this.dx;
             let bottomObstacleYPos = p.y + this.top.ch + this.gap;
-            if(character.x + character.w/2 > (p.x-30) && character.x - character.w/2 < (p.x-30) + this.top.w && character.y + character.ch/2 >p.y && character.y - character.ch/4<p.y + this.top.ch){
-                state.current = state.gameOver;
+            if(character.x + character.w/2 > (p.x-30) && character.x - character.w/2 < (p.x-30) + this.top.w && character.y + character.ch/2 >p.y && character.y - character.ch/2<p.y + this.top.ch){
+                // state.current = state.gameOver;
             }
             if(character.x  + character.w/2 > p.x && character.x - character.w/2 < p.x+this.bottom.w && character.y + character.ch/2 >bottomObstacleYPos && character.y - character.ch/2< bottomObstacleYPos + this.bottom.ch){
-                //state.current = state.gameOver;
+                // state.current = state.gameOver;
             }
             if(p.x + this.top.w <= 0){
                 this.position.shift();
@@ -151,7 +171,7 @@ const state = {
     gameOver: 2
 }
 
-cvs.addEventListener("click",function(evt){
+cvs.addEventListener("touchstart",function(evt){
     switch(state.current){
         case state.getReady:
             state.current = state.game;
